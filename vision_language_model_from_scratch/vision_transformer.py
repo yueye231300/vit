5aa880a265acf8126257b779afc670f2a6c1dd05
yuyue <1650958692@qq.com>
2026-02-21 23:57:24 +0800

    we abandon the usage of the linear

---
+++ vision_language_model_from_scratch/vision_transformer.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import torch 
+import torch.nn as nn 
+
+a = torch.randn(1,2)
+linear = nn.Linear(2,6) # linear is only used in the last channel 
+b = linear(a)
+print(b.shape)
+
+print(1)
