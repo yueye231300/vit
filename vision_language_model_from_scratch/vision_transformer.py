8db2c4c424f8ce200cc1d696a6cdc77b349c5909
yuyue <1650958692@qq.com>
2026-02-24 18:57:08 +0800

    the initial commit message for the changes

--- vision_language_model_from_scratch/vision_transformer.py
+++ vision_language_model_from_scratch/vision_transformer.py
@@ -2,9 +2,11 @@ import numpy as np
 import torch 
 import torch.nn as nn 
 
-a = torch.randn(1,2)
-linear = nn.Linear(2,6) # linear is only used in the last channel 
-b = linear(a)
-print(b.shape)
+# a = torch.randn(1,2)
+# linear = nn.Linear(2,6) 
+# # linear is only used in the last channel 
+# b = linear(a)
+# print(b.shape)
 
-print(1)
+
+
